ee5239ee802717882831fcee0f64df6ca6450b12
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2023-01-14 01:26:17 -0800

    Misc cleanup
    * Added and applied prettier to project
    * Upgraded package-lock.json from npm

--- app/js/util.test.js
+++ app/js/util.test.js
@@ -4,181 +4,191 @@ var util = require('./util.js');
 var xhrMock = require('xhr-mock');
 xhrMock.setup();
 
-describe('util', function() {
+describe('util', function () {
+  it('must be defined', function () {
+    assert.isDefined(util);
+  });
 
-    it('must be defined', function() {
-        assert.isDefined(util);
+  describe('util.formatTime', function () {
+    it('must be defined', function () {
+      assert.isDefined(util.formatTime);
     });
 
-    describe('util.formatTime', function() {
+    var tests = [
+      {
+        given: 120,
+        expected: '2:00',
+      },
+      {
+        given: 119,
+        expected: '1:59',
+      },
+      {
+        given: 121,
+        expected: '2:01',
+      },
+      {
+        given: 0,
+        expected: '0:00',
+      },
+    ];
 
-        it('must be defined', function() {
-            assert.isDefined(util.formatTime);
-        });
+    tests.forEach(function (test) {
+      it('must return ' + test.expected + ' given ' + test.given, function () {
+        assert.strictEqual(util.formatTime(test.given), test.expected);
+      });
+    });
+  });
 
-        var tests = [
-            {
-                given: 120,
-                expected: '2:00'
-            },
-            {
-                given: 119,
-                expected: '1:59'
-            },
-            {
-                given: 121,
-                expected: '2:01'
-            },
-            {
-                given: 0,
-                expected: '0:00'
-            }
-        ];
-
-        tests.forEach(function(test) {
-            it('must return '+test.expected+' given '+test.given, function() {
-                assert.strictEqual(util.formatTime(test.given), test.expected);
-            });
-        });
+  describe('util.isAvailableMapHash', function () {
+    it('must be defined', function () {
+      assert.isDefined(util.isAvailableMapHash);
     });
 
-    describe('util.isAvailableMapHash', function() {
+    var mockMaps = {
+      stalingrad: {
+        hash: '#stalingrad',
+        selectIndex: 0,
+      },
+      other: {
+        hash: '#other',
+        selectIndex: 1,
+      },
+    };
 
-        it('must be defined', function() {
-            assert.isDefined(util.isAvailableMapHash);
-        });
+    var tests = [
+      {
+        given: '#stalingrad',
+        expected: true,
+      },
+      {
+        given: '#none',
+        expected: false,
+      },
+    ];
 
-        var mockMaps = {
-            stalingrad: {
-                hash: '#stalingrad',
-                selectIndex: 0
-            },
-            other: {
-                hash: '#other',
-                selectIndex: 1
-            }
-        };
+    tests.forEach(function (test) {
+      it('must return ' + test.expected + ' given ' + test.given, function () {
+        assert.strictEqual(util.isAvailableMapHash(test.given, mockMaps), test.expected);
+      });
+    });
+  });
 
-        var tests = [
-            {
-                given: '#stalingrad',
-                expected: true
-            },
-            {
-                given: '#none',
-                expected: false
-            }
-        ]
-
-        tests.forEach(function(test) {
-            it('must return '+test.expected+' given '+test.given, function() {
-                assert.strictEqual(util.isAvailableMapHash(test.given, mockMaps), test.expected);
-            });
-        });
+  describe('util.getSelectedMapConfig', function () {
+    it('must be defined', function () {
+      assert.isDefined(util.getSelectedMapConfig);
     });
 
-    describe('util.getSelectedMapConfig', function() {
+    var mockMaps = {
+      stalingrad: {
+        hash: '#stalingrad',
+        selectIndex: 0,
+      },
+      other: {
+        hash: '#other',
+        selectIndex: 1,
+      },
+    };
 
-        it('must be defined', function() {
-            assert.isDefined(util.getSelectedMapConfig);
-        });
+    var tests = [
+      {
+        given: '#stalingrad',
+        expected: 'stalingrad',
+      },
+      {
+        given: '#other',
+        expected: 'other',
+      },
+      {
+        given: '#none',
+        expected: 'stalingrad',
+      },
+    ];
 
-        var mockMaps = {
-            stalingrad: {
-                hash: '#stalingrad',
-                selectIndex: 0
-            },
-            other: {
-                hash: '#other',
-                selectIndex: 1
-            }
-        };
+    tests.forEach(function (test) {
+      it('must return ' + test.expected + ' given ' + test.given, function () {
+        assert.strictEqual(util.getSelectedMapConfig(test.given, mockMaps), mockMaps[test.expected]);
+      });
+    });
+  });
 
-        var tests = [
-            {
-                given: '#stalingrad',
-                expected: 'stalingrad'
-            },
-            {
-                given: '#other',
-                expected: 'other'
-            },
-            {
-                given: '#none',
-                expected: 'stalingrad'
-            }
-        ]
-
-        tests.forEach(function(test) {
-            it('must return '+test.expected+' given '+test.given, function() {
-                assert.strictEqual(util.getSelectedMapConfig(test.given, mockMaps), mockMaps[test.expected]);
-            });
-        });
+  describe('util.defaultPopulateArray', function () {
+    it('must be defined', function () {
+      assert.isDefined(util.defaultPopulateArray);
     });
 
-    describe('util.defaultPopulateArray', function() {
+    var tests = [
+      {
+        given: {
+          val: 300,
+          count: 5,
+        },
+        expectedLength: 5,
+      },
+      {
+        given: {
+          val: 300,
+          count: 0,
+        },
+        expectedLength: 0,
+      },
+    ];
 
-        it('must be defined', function() {
-            assert.isDefined(util.defaultPopulateArray);
-        });
-
-        var tests = [
-            {
-                given: {
-                    val: 300,
-                    count: 5
-                },
-                expectedLength: 5
-            },
-            {
-                given: {
-                    val: 300,
-                    count: 0
-                },
-                expectedLength: 0
-            }
-        ]
-
-        tests.forEach(function(test) {
-            it('must return array of length '+test.expectedLength+' given '+test.given.val+' and '+test.given.count, function() {
-                assert.lengthOf(util.defaultPopulateArray(test.given.val, test.given.count), test.expectedLength);
-            });
-        });
-    });
-
-    describe('util.formatFlightLegMarker', function() {
-
-        it('must be defined', function() {
-            assert.isDefined(util.formatFlightLegMarker);
-        });
-
-        var expected = '<div style="background: rgba(100, 100, 100, .75); transform: rotate(26deg);">300.0km | 116&deg;/296&deg; <br/> 300kph | ETE 60:00</div>';
-        var given = {
-            distance: 300,
-            heading: 116,
-            speed: 300,
-            time: '60:00'
+    tests.forEach(function (test) {
+      it(
+        'must return array of length ' + test.expectedLength + ' given ' + test.given.val + ' and ' + test.given.count,
+        function () {
+          assert.lengthOf(util.defaultPopulateArray(test.given.val, test.given.count), test.expectedLength);
         }
- 
-        it('must return '+expected+' given '+given.distance+', '+given.heading+', '+given.speed+' and '+given.time, function() {
-            assert.strictEqual(util.formatFlightLegMarker(given.distance, given.heading, given.speed, given.time, 'metric'), expected);
-        });
+      );
+    });
+  });
+
+  describe('util.formatFlightLegMarker', function () {
+    it('must be defined', function () {
+      assert.isDefined(util.formatFlightLegMarker);
     });
 
-    describe('util.buildGetXhr', function() {
+    var expected =
+      '<div style="background: rgba(100, 100, 100, .75); transform: rotate(26deg);">300.0km | 116&deg;/296&deg; <br/> 300kph | ETE 60:00</div>';
+    var given = {
+      distance: 300,
+      heading: 116,
+      speed: 300,
+      time: '60:00',
+    };
 
-        it('must be defined', function() {
-            assert.isDefined(util.buildGetXhr);
-        });
+    it(
+      'must return ' +
+        expected +
+        ' given ' +
+        given.distance +
+        ', ' +
+        given.heading +
+        ', ' +
+        given.speed +
+        ' and ' +
+        given.time,
+      function () {
+        assert.strictEqual(
+          util.formatFlightLegMarker(given.distance, given.heading, given.speed, given.time, 'metric'),
+          expected
+        );
+      }
+    );
+  });
+
+  describe('util.buildGetXhr', function () {
+    it('must be defined', function () {
+      assert.isDefined(util.buildGetXhr);
     });
+  });
 
-    describe('util.buildSyncGetXhr', function() {
-
-        it('must be defined', function() {
-            assert.isDefined(util.buildSyncGetXhr);
-        });
+  describe('util.buildSyncGetXhr', function () {
+    it('must be defined', function () {
+      assert.isDefined(util.buildSyncGetXhr);
     });
+  });
 
-    xhrMock.teardown();
-
+  xhrMock.teardown();
 });
